f3a474c5c056a0700e4d91c2c4a73663194c7d4b
Zhalgas <zhalgas99@outlook.com>
2023-04-19 21:09:17 +0300

    unique_ptr -> shared_ptr

--- include/net/address/ip.hpp
+++ include/net/address/ip.hpp
@@ -20,46 +20,54 @@ namespace NAsync {
     public:
         // Constructors
         explicit TIPv4Address(in_addr rawAddr) noexcept;
-        TIPv4Address(const TIPv4Address&) = delete;
-        TIPv4Address& operator=(const TIPv4Address&) = delete;
-        TIPv4Address(TIPv4Address&&) noexcept;
-        TIPv4Address& operator=(TIPv4Address&&) noexcept;
-        ~TIPv4Address();
 
         // Static factories
         static TResult<TIPv4Address> FromString(const std::string& s) noexcept;
-        static TIPv4Address Localhost() noexcept;
-        static TIPv4Address Any() noexcept;
+
+        static inline TIPv4Address Localhost() noexcept {
+            return *FromString("127.0.0.1");
+        }
+
+        static inline TIPv4Address Any() noexcept {
+            return *FromString("0.0.0.0");
+        }
 
         // Insights
-        const in_addr& Raw() const noexcept;
+        inline const in_addr& Raw() const noexcept {
+            return *Raw_;
+        }
+
         std::string ToString() const noexcept override;
 
     private:
-        std::unique_ptr<in_addr> RawAddr_;
+        std::shared_ptr<in_addr> Raw_;
     };
 
     class TIPv6Address : public IAddress {
     public:
         // Constructors
         explicit TIPv6Address(in6_addr rawAddr) noexcept;
-        TIPv6Address(const TIPv6Address&) = delete;
-        TIPv6Address& operator=(const TIPv6Address&) = delete;
-        TIPv6Address(TIPv6Address&&) noexcept;
-        TIPv6Address& operator=(TIPv6Address&&) noexcept;
-        ~TIPv6Address();
 
         // Static factories
         static TResult<TIPv6Address> FromString(const std::string& s) noexcept;
-        static TIPv6Address Localhost() noexcept;
-        static TIPv6Address Any() noexcept;
+
+        static inline TIPv6Address Localhost() noexcept {
+            return *FromString("::1");
+        }
+
+        static inline TIPv6Address Any() noexcept {
+            return *FromString("::");
+        }
 
         // Insights
-        const in6_addr& Raw() const noexcept;
+        inline const in6_addr& Raw() const noexcept {
+            return *Raw_;
+        }
+
         std::string ToString() const noexcept override;
 
     private:
-        std::unique_ptr<in6_addr> RawAddr_;
+        std::shared_ptr<in6_addr> Raw_;
     };
 
 } // namespace NAsync
--- src/net/address/ip.cpp
+++ src/net/address/ip.cpp
@@ -7,20 +7,12 @@ namespace NAsync {
 
     // TIPv4Address
     TIPv4Address::TIPv4Address(in_addr addr) noexcept
-        : RawAddr_ {new in_addr{std::move(addr)}}
+        : Raw_ {new in_addr{std::move(addr)}}
     {}
-
-    TIPv4Address::TIPv4Address(TIPv4Address&&) noexcept = default;
-    TIPv4Address& TIPv4Address::operator=(TIPv4Address&&) noexcept = default;
-    TIPv4Address::~TIPv4Address() = default;
-
-    const in_addr& TIPv4Address::Raw() const noexcept {
-        return *RawAddr_;
-    }
 
     std::string TIPv4Address::ToString() const noexcept {
         char buffer[INET_ADDRSTRLEN];
-        const char* str = inet_ntop(AF_INET, RawAddr_.get(), buffer, sizeof(buffer));
+        const char* str = inet_ntop(AF_INET, Raw_.get(), buffer, sizeof(buffer));
         VERIFY_SYSCALL(str != nullptr);
         return std::string{str};
     }
@@ -35,31 +27,14 @@ namespace NAsync {
         return std::error_code{EAFNOSUPPORT, std::system_category()};
     }
 
-    TIPv4Address TIPv4Address::Localhost() noexcept {
-        return *FromString("127.0.0.1");
-    }
-
-    TIPv4Address TIPv4Address::Any() noexcept {
-        return *FromString("0.0.0.0");
-    }
-
     // TIPv6Address
     TIPv6Address::TIPv6Address(in6_addr addr) noexcept
-        : RawAddr_ {new in6_addr{std::move(addr)}}
+        : Raw_ {new in6_addr{std::move(addr)}}
     {}
-
-    TIPv6Address::TIPv6Address(TIPv6Address&&) noexcept = default;
-    TIPv6Address& TIPv6Address::operator=(TIPv6Address&&) noexcept = default;
-
-    TIPv6Address::~TIPv6Address() = default;
-
-    const in6_addr& TIPv6Address::Raw() const noexcept {
-        return *RawAddr_;
-    }
 
     std::string TIPv6Address::ToString() const noexcept {
         char buffer[INET6_ADDRSTRLEN];
-        const char* str = inet_ntop(AF_INET6, RawAddr_.get(), buffer, sizeof(buffer));
+        const char* str = inet_ntop(AF_INET6, Raw_.get(), buffer, sizeof(buffer));
         VERIFY_SYSCALL(str != nullptr);
         return std::string{str};
     }
@@ -72,14 +47,6 @@ namespace NAsync {
         }
 
         return std::error_code{errno, std::system_category()};
-    }
-
-    TIPv6Address TIPv6Address::Localhost() noexcept {
-        return *FromString("::1");
-    }
-
-    TIPv6Address TIPv6Address::Any() noexcept {
-        return *FromString("::");
     }
 
 } // namespace NAsync
