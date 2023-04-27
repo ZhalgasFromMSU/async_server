7af84c4d89ea5dd7f5a1f56ce4c93a54d8cea249
Zhalgas <zhalgas99@outlook.com>
2023-04-27 16:39:19 +0000

    fix lock-free queue

--- include/util/queue.hpp
+++ include/util/queue.hpp
@@ -6,13 +6,24 @@
 
 namespace NAsync {
 
-    template<typename T, size_t MaxSize = 1024>
+    template<typename T, size_t MaxSize>
         requires (MaxSize > 0)
     class TQueue {
     public:
+        //
+        //    Almost atomic queue. It has no locking, but it can fail spuriously in following cases
+        //        1) Tail_ approaches Head_ from behind (possible beacuse it is ring buffer). Pop() was called right before Push()
+        //           Head_ moved one step forward, but optional, containing value didn't destroy object. In this case new element can't be added, although
+        //           we will have enough space to hold it just in a moment
+        //        2) Same with opposite situation. Head_ approaches Tail_, but it can't place new object yet, because previous call to Pop() didn't finish
+        //
+        //    Head_ ->            Tail_ ->
+        //      |                   |
+        // nil val val val val val nil nil nil
+        //
         template<typename... TArgs>
-        bool Push(TArgs&&... args) {
-            size_t place = MoveHead();
+        bool Push(TArgs&&... args) { // put element to the end of a queue
+            size_t place = MoveTail();
             if (place == BufSize) {
                 return false;
             }
@@ -21,12 +32,15 @@ namespace NAsync {
             return true;
         }
 
-        std::optional<T> Pop() {
-            size_t place = MoveTail();
+        std::optional<T> Pop() { // pop element from top of the queue
+            size_t place = MoveHead();
             if (place == BufSize) {
                 return std::nullopt;
             }
-            return std::move(RingBuffer_[place]);
+            std::optional<T> ret = std::move(RingBuffer_[place]);
+            RingBuffer_[place].reset();
+            ContainsValue_[place] = false;
+            return ret;
         }
 
         // Head_ == Tail_ - there is no elements
@@ -37,32 +51,32 @@ namespace NAsync {
             if (curTail >= curHead) {
                 return curTail - curHead;
             } else {
-                return MaxSize - curHead + curTail;
+                return BufSize - curHead + curTail;
             }
         }
 
     private:
-        size_t MoveHead() {
-            size_t curVal = Head_;
+        size_t MoveTail() {
+            size_t curVal = Tail_;
             while (true) {
-                size_t newVal = (curVal == 0 ? BufSize - 1 : curVal - 1);
-                if (Tail_ == newVal) {
+                size_t newVal = (curVal + 1) % BufSize;
+                if (Head_ == newVal || ContainsValue_[curVal]) {
                     return BufSize;
                 }
-                if (Head_.compare_exchange_weak(curVal, newVal)) {
+                if (Tail_.compare_exchange_weak(curVal, newVal)) {
                     return curVal;
                 }
             }
         }
 
-        size_t MoveTail() {
-            size_t curVal = Tail_;
+        size_t MoveHead() {
+            size_t curVal = Head_;
             while (true) {
-                if (Head_ == curVal || !ContainsValue_[curVal]) {
+                if (Tail_ == curVal || !ContainsValue_[curVal]) {
                     return BufSize;
                 }
-                size_t newVal = (curVal == 0 ? BufSize - 1 : curVal - 1);
-                if (Tail_.compare_exchange_weak(curVal, newVal)) {
+                size_t newVal = (curVal + 1) % BufSize;
+                if (Head_.compare_exchange_weak(curVal, newVal)) {
                     return curVal;
                 }
             }
