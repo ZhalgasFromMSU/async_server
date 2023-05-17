b82501dc09b5161e15196b5b61727eb39edabc12
Zhalgas <zhalgas99@outlook.com>
2023-05-17 19:23:20 +0000

    use opensource http parser

--- include/http/message.hpp
+++ include/http/message.hpp
@@ -1,50 +1,9 @@
 #pragma once
 
-#include <string>
-#include <vector>
+#include <httpparser/request.h>
+#include <httpparser/response.h>
 
 namespace NAsync {
-
-    enum class EHttpMethod {
-        kGet,
-        kHead,
-        kPost,
-        kPut,
-        kDelete,
-        kConnect,
-        kOptions,
-        kTrace,
-        kPatch,
-    };
-
-    struct THttpRequest {
-        // First line
-        EHttpMethod Method = EHttpMethod::kGet;
-        std::string Path = "/";
-        std::vector<std::pair<std::string, std::string>> Cgi;
-        int HttpVersionMajor = 1;
-        int HttpVersionMinor = 1;
-        // Headers
-        std::vector<std::pair<std::string, std::string>> Headers;
-        // Body
-        std::string Body;
-
-        // Methods
-        std::string Serialize() const;
-    };
-
-    struct THttpResponse {
-        // First line
-        int Status;
-        int StatusMessage;
-        int HttpVersionMajor;
-        int HttpVersionMinor;
-        // Headers
-        std::vector<std::pair<std::string, std::string>> Headers;
-        // Body
-        std::string Body;
-
-        // Methods
-        static THttpResponse FromString(const std::string& s);
-    };
+    using THttpRequest = httpparser::Request;
+    using THttpResponse = httpparser::Response;
 } // namespace NAsync
